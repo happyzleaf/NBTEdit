7d0f7385166d76b8de826804ffe4ac70e21599e6
Vincenzo (happyzleaf) <happyzleaf@hotmail.com>
2018-07-30 17:05:09 +0200

    Made the text look the same as pixelmon's overlay

--- src/main/java/com/mcf/davidee/nbtedit/forge/Overlay.java
+++ src/main/java/com/mcf/davidee/nbtedit/forge/Overlay.java
@@ -3,11 +3,14 @@ package com.mcf.davidee.nbtedit.forge;
 import com.pixelmonmod.pixelmon.client.gui.GuiHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.GL11;
 
 /**
  * @author happyzleaf
@@ -16,8 +19,13 @@ import org.lwjgl.input.Keyboard;
 public class Overlay extends Gui {
 	private static final ResourceLocation RESOURCE = new ResourceLocation("nbtedit", "textures/gui/team_overlay.png");
 	
+	/**
+	 * The unicode flag is set to true to copy better the style of pixelmon's overlay.
+	 * @see com.pixelmonmod.pixelmon.client.gui.GuiPixelmonOverlay#onRenderGameOverlay(RenderGameOverlayEvent.Pre)
+	 */
 	@SubscribeEvent
 	public void onRenderGameOverlay(RenderGameOverlayEvent.Pre event) {
+		Minecraft.getMinecraft().fontRenderer.setUnicodeFlag(true);
 		int scaledWidth = event.getResolution().getScaledWidth();
 		int scaledHeight = event.getResolution().getScaledHeight();
 		if (ClientProxy.NBTEditTeam.getKeyCode() != Keyboard.KEY_NONE) {
@@ -25,5 +33,6 @@ public class Overlay extends Gui {
 			Minecraft.getMinecraft().renderEngine.bindTexture(RESOURCE);
 			GuiHelper.drawImageQuad(scaledWidth - 105, scaledHeight - 30, 20, 20, 0, 0, 1, 1, this.zLevel);
 		}
+		Minecraft.getMinecraft().fontRenderer.setUnicodeFlag(false);
 	}
 }
